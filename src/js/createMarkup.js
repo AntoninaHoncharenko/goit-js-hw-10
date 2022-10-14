3a334c5de333e887556624590d79111ff9fb56f6
AntoninaHoncharenko <g.antonina.vl@gmail.com>
2022-10-14 19:39:17 +0300

    add styles

--- src/js/createMarkup.js
+++ src/js/createMarkup.js
@@ -1,29 +1,28 @@
 export function createMarkupList(data) {
   const { name, flags } = data;
-  return `<li>
-  <img src="${flags.svg}" alt="flag" width="40px" height=""30px/>
-  <p>${name.official}</p>
+  return `<li class="country-item"> 
+  <img src="${flags.svg}" alt="flag" width="60px" height=""40px/>
+  <p class="name">${name.official}</p>
 </li>`;
 }
 
 export function createMarkupElement(data) {
   const { name, capital, population, flags, languages } = data[0];
 
-  return `<h2>
+  return `
   <img src="${flags.svg}" alt="flag" width="200px" height=""100px/>
-  <p>${name.official}</p>
-</h2>
+  <h2 class="card-name">${name.official}</h2>
 <ul>
-  <li>
-    <p>Capital:</p>
+  <li class="item">
+    <p class="subtitle">Capital:</p>
     <span>${capital}</span>
   </li>
-  <li>
-    <p>Population:</p>
+  <li class="item">
+    <p class="subtitle">Population:</p>
     <span>${population}</span>
   </li>
-  <li>
-    <p>Languages:</p>
+  <li class="item">
+    <p class="subtitle">Languages:</p>
     <span>${Object.values(languages).join(', ')}</span>
   </li>
 </ul>`;
